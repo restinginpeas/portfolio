0e729fcf14b30d4650641d4b80fcf6f312eb0967
restinginpeas <115091522+restinginpeas@users.noreply.github.com>
2024-06-26 11:07:37 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hi, I'm Junas!</title>
+    
+</head>
+<body>
+    <div class="clear-fix">
+        <header>
+            <div class="box-wrap">
+                <div class="head-con">
+
+                </div>
+            </div>
+        </header>
+        <div id="navig">
+            <div class="box-wrap">
+                <div class="navg-con">
+
+                </div>
+            </div>
+        </div>
+        <div id="hero">
+            <div class="box-wrap">
+                <div class="hero-con"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
